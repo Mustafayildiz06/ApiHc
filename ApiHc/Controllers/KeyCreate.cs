1acdc3c13db525116295aafc2e500d56d6556438
mustafa.yildiz <mustafa.yildiz@DESKTOP-R35LEN7>
2022-11-01 00:05:11 +0300

    Add project files.

---
+++ ApiHc/Controllers/KeyCreate.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiHc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KeyCreate : ControllerBase
+    {
+
+
+
+    }
+}
